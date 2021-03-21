bbe03efce0d1a224d9d9a20d8a5d95d081155ca0
Havockeyan <karthihavoc121@gmail.com>
2021-03-21 19:48:42 +0530

    1 st program

--- gowtham1.c
+++ gowtham1.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<math.h>
 int main(){
     float v,d,q,l,V;
     printf("Enter the values of the following:");
@@ -9,9 +10,15 @@ int main(){
     printf("Discharge:");
     scanf("%f",&q);
     printf("Length of pipe:");
-    scanf("%f,&l");
+    scanf("%f",&l);
     float a=(3.14/4)*d*d;
     V=q/a;
     printf("\n%f",V);
-    //hii
+    float r=V*d/v;
+    printf("\n%f",r);
+    //float s=1/4;
+    float f=0.079/pow(r,0.25);
+    printf("\n%f",f);
+    float h=(4*f*l*V*V)/(d*2*9.81);
+    printf("\n%f",h);
 }
